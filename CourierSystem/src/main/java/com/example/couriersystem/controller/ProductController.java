33b137e2436f915e03d3da443bfbc5d35db0ddd9
DeagleM <W0258876@campus.nscc.ca>
2024-11-18 19:05:56 -0400

    Modified endpoint logic
    Added put, delete. Changed url to api/v1/product (no s)

--- CourierSystem/src/main/java/com/example/couriersystem/controller/ProductController.java
+++ CourierSystem/src/main/java/com/example/couriersystem/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.example.couriersystem.controller;
 import com.example.couriersystem.model.Product; //for test purpose
 import com.example.couriersystem.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,7 +12,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/api/v1/product")
 public class ProductController {
 
     private final ProductRepository productRepository;
@@ -24,7 +26,11 @@ public class ProductController {
     public List<Product> getAllProducts() {return productRepository.findAll();}
 
     @GetMapping("/{id}")
-    public void getProductById(@PathVariable Long id) {}
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return productRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
+    }
 
     @PostMapping
     public Product createProduct(@RequestBody Product product) {
@@ -32,8 +38,26 @@ public class ProductController {
     }
 
     @PutMapping("/{id}")
-    public void updateProduct(@PathVariable Long id) {}
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
+        if (productDetails.getName() == null || productDetails.getDescription() == null || productDetails.getPrice() < 0 || productDetails.getQuantity() < 0) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        return productRepository.findById(id).map(product -> {
+            product.setName(productDetails.getName());
+            product.setDescription(productDetails.getDescription());
+            product.setPrice(productDetails.getPrice());
+            product.setQuantity(productDetails.getQuantity());
+            return ResponseEntity.ok(product);
+        }).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
+    }
 
     @DeleteMapping("/{id}")
-    public void deleteProduct(@PathVariable Long id) {}
+    public String deleteProduct(@PathVariable Long id) {
+        if (productRepository.existsById(id)) {
+            productRepository.deleteById(id);
+            return "Product deleted successfully";
+        } else {
+            return "Product not found";
+        }
+    }
 }
